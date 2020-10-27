47d800db93d39b4ff170d237bea735b065ca7bb4
Ak623py <65203191+Ak623py@users.noreply.github.com>
2020-10-27 10:00:17 +0900

    Revert "section2-q1 changed"
    This reverts commit bad4d16ac12c3bd59eebd96f92cab02e9b71f9d1.

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,17 +1,6 @@
 // 小手調べ
 function setup(){
-createCanvas(100,100);
-noFill();
   for(let i = 0; i < 10; i++){
-
-  let r = i * 10 + 10
-  ellipse(50, 50, r, r);
-  if(i < 4){
-    stroke(0, 0, 255);
-  }
-  else{
-    stroke(255, 0, 0);
-  }
     // BLANK[1]
   }
 }
