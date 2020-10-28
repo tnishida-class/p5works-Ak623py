b93d33c2b59e2deda6b2dbfb1fba703a39498962
Ak623py <65203191+Ak623py@users.noreply.github.com>
2020-10-28 17:06:25 +0900

    Submit section2-q1

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -3,15 +3,15 @@ function setup(){
 createCanvas(100,100);
 noFill();
   for(let i = 0; i < 10; i++){
-
   let r = i * 10 + 10
-  ellipse(50, 50, r, r);
-  if(i < 4){
+  if(i < 5){
     stroke(0, 0, 255);
   }
   else{
     stroke(255, 0, 0);
   }
+  ellipse(50, 50, r, r);
+
     // BLANK[1]
   }
 }
